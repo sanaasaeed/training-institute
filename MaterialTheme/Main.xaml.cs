edf87fe3fb6f5ccd06354077cb07dd553481db07
sanaasaeed <sunfunpower14@gmail.com>
2020-05-30 01:11:33 +0500

    Database working fine

--- MaterialTheme/Main.xaml.cs
+++ MaterialTheme/Main.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,8 +18,76 @@ namespace MaterialTheme {
   /// Interaction logic for Main.xaml
   /// </summary>
   public partial class Main : Page {
+    public List<String> unassigned = new List<String>();
+    public List<String> studentsWithNoCourse = new List<String>();
+    public int noOfStudents { get; set; }
+    public int noOfTrainers { get; set; }
+    public int noOfCourses { get; set; }
     public Main() {
       InitializeComponent();
+      var context = new TrainingDBContext();
+      // Reports about no of things in Database
+      var students = context.Students.Select(std => std);
+      noOfStudents = students.Count();
+      var trainers = context.Trainers.Select(tra => tra);
+      noOfTrainers = trainers.Count();
+      var courses = context.Courses.Select(cours => cours);
+      noOfCourses = courses.Count();
+
+
+      // UNASSIGNED COURSES
+      var unassignedCourses = context.Courses.Where(cour => cour.TrainerId == null).Select(cour => new  {
+        CourseName = cour.Title
+      });
+      // Unassigned Courses in combobox
+      foreach (var unassignedCourse in unassignedCourses) {
+        unassigned.Add(unassignedCourse.CourseName);
+      }
+      UnassignedCourse.ItemsSource = unassigned;
+      this.DataContext = this;
+
+
+      // STUDENTS TAKING NO COURSES
+      var result = (from student in context.Students
+        where
+          !
+            (from studentcourse in context.StudentCourses
+              select new {
+                studentcourse.StudentId
+              }).Contains(new {StudentId = student.StudentId})
+        select new {
+          student.StudentId
+        }).ToList();
+
+      MessageBox.Show(result.Count().ToString());
+      StudentCombo.ItemsSource = result;
+    }
+
+
+
+
+
+    private void OnUnassignedSelectionChange(object sender, SelectionChangedEventArgs e) {
+      // Updating Data Grid 
+      var context = new TrainingDBContext();
+      var selectedCourse = UnassignedCourse.SelectedValue.ToString();
+      var selectedQuery = context.Courses.Where(cour => cour.Title == selectedCourse).Select(cour => new {
+        cour.Code, cour.Title, cour.Description, cour.TrainerId
+      }).ToList();
+      DataGridMain.ItemsSource = selectedQuery;
+    }
+
+    private void StudentCombo_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+      // Updating Data Grid 
+      // var context = new TrainingDBContext();
+      // var selectedCourse = StudentCombo.SelectedValue.ToString();
+      // var selectedQuery = context.Courses.Where(cour => cour.Title == selectedCourse).Select(cour => new {
+      //   cour.Code,
+      //   cour.Title,
+      //   cour.Description,
+      //   cour.TrainerId
+      // }).ToList();
+      // DataGridMain.ItemsSource = selectedQuery;
     }
   }
 }
--- MaterialTheme/Students.xaml.cs
+++ MaterialTheme/Students.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,18 +21,35 @@ namespace MaterialTheme {
   /// </summary>
   public partial class Students : Page {
     public Students() {
-      List<Me> listme = new List<Me>() {
-        new Me() {
-          Name = "Sana Saeed",
-          Age = 23
-        },
-        new Me() {
-          Name = "Musfirah Mansoor",
-          Age = 22
-        }
-      };
+      
       InitializeComponent();
-      ListStudents.ItemsSource = listme;
+      List<String> nameOfStudents = new List<String>();
+       List<String> courseList = new List<String>();
+      var context = new TrainingDBContext();
+      // Retriving data from database
+      var listOfStudents = (from student in context.Students
+        select new {FirstName = student.FirstName, LastName = student.LastName,student.RegNo,student.PhoneNo, student.Email, student.Address,student.DateOfBirth
+        }).ToList();
+      // Binding data
+      foreach (var student in listOfStudents) {
+        nameOfStudents.Add(student.FirstName);
+      }
+
+      var coursesList = (from course in context.Courses
+        select new {
+          CourseTitle = course.Title
+        }).ToList();
+
+      foreach (var courses in coursesList) {
+        courseList.Add(courses.CourseTitle);
+      }
+      ListStudents.ItemsSource = listOfStudents;
+      AssignStdCombo.ItemsSource = nameOfStudents;
+      DeleteStdCombo.ItemsSource = nameOfStudents;
+      AssignCourseCombo.ItemsSource = courseList;
+
+      // Assigning Course to student
+
     }
 
     private void BrowseBtnClicked(object sender, RoutedEventArgs e) {
@@ -45,10 +63,65 @@ namespace MaterialTheme {
           Photo.Source = new BitmapImage(fileUri);
         }
     }
+
+    private void BtnAdd_OnClick(object sender, RoutedEventArgs e) {
+      var context = new TrainingDBContext();
+      if (FirstNameTextBox.Text == "" || LastNameTextBox.Text == "" || AddressTextBox.Text == "" ||
+          DateOfBirthPicker.Text == "" || EmailTextBox.Text == "" || PhoneNumberTextBox.Text == "" ||
+          RegTextBox.Text == "") {
+        MessageBox.Show("All fields are required");
+      }
+      else {
+        var studentObj = new Student() {
+          FirstName = FirstNameTextBox.Text,
+          LastName = LastNameTextBox.Text,
+          Address = AddressTextBox.Text,
+          DateOfBirth = DateOfBirthPicker.Text,
+          Email = EmailTextBox.Text,
+          PhoneNo = PhoneNumberTextBox.Text,
+          RegNo = RegTextBox.Text,
+          PhotoSrc = Photo.Source.ToString()
+        };
+
+        context.Students.Add(studentObj);
+        context.SaveChanges();
+     
+      }
+    }
+
+    private void Refresh(object sender, RoutedEventArgs e) {
+      List<String> nameOfStudents = new List<String>();
+      List<String> courseList = new List<String>();
+      var context = new TrainingDBContext();
+      // Retriving data from database
+      var listOfStudents = (from student in context.Students
+        select new {
+          FirstName = student.FirstName,
+          LastName = student.LastName,
+          student.RegNo,
+          student.PhoneNo,
+          student.Email,
+          student.Address,
+          student.DateOfBirth
+        }).ToList();
+      // Binding data
+      foreach (var student in listOfStudents) {
+        nameOfStudents.Add(student.FirstName);
+      }
+
+      var coursesList = (from course in context.Courses
+        select new {
+          CourseTitle = course.Title
+        }).ToList();
+
+      foreach (var courses in coursesList) {
+        courseList.Add(courses.CourseTitle);
+      }
+      ListStudents.ItemsSource = listOfStudents;
+      AssignStdCombo.ItemsSource = nameOfStudents;
+      DeleteStdCombo.ItemsSource = nameOfStudents;
+      AssignCourseCombo.ItemsSource = courseList;
+    }
   }
 
-  public class Me {
-    public string Name { get; set; }
-    public int Age { get; set; }
-  }
 }
